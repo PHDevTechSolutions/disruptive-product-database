2df63d0c704ea1ddcdb20cbd6edfdf13c1657d08
RonRon <aaronmiguelespiritu123@gmail.com>
2026-02-15 22:48:31 +0800

    AAron

--- app/add-product/page.tsx
+++ app/add-product/page.tsx
@@ -1389,41 +1389,6 @@ const logisticsPayload = {
     }
   };
 
-  const clearTechnicalSpecifications = () => {
-    setTechnicalSpecs([
-      {
-        id: "",
-        title: "",
-        specs: [
-          {
-            specId: "",
-            unit: "",
-
-            isRanging: false,
-            isSlashing: false,
-            isDimension: false,
-            isIPRating: false,
-
-            value: "",
-            rangeFrom: "",
-            rangeTo: "",
-
-            slashValues: [""],
-
-            length: "",
-            width: "",
-            height: "",
-
-            ipFirst: "",
-            ipSecond: "",
-          },
-        ],
-        units: [],
-      },
-    ]);
-  };
-
-
   const handleSaveProduct = async () => {
     if (saving) return;
     try {
@@ -1515,9 +1480,6 @@ const logisticsPayload = {
       });
 
       toast.success("Product saved successfully");
-
-      clearTechnicalSpecifications();
-
       router.push("/products");
 
       // 🚀 background upload (wag hintayin)
