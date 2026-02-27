afd3d4802d896a20e566d4f54b873350efb6e5f0
ron80-afk <aaronmiguelespiritu123@gmail.com>
2026-02-27 18:02:50 +0800

    Aaron

--- utils/supabase-ticket.ts
+++ utils/supabase-ticket.ts
@@ -1,7 +1,10 @@
-// utils/supabase.js
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL_TICKET;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Missing Supabase environment variables");
+}
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
