520cbd2531c44427373ab02b525f5cb94050031c
ron80-afk <aaronmiguelespiritu123@gmail.com>
2026-03-26 11:10:53 +0800

    Aaron

--- components/spf-request-fetch-version-history.tsx
+++ components/spf-request-fetch-version-history.tsx
@@ -93,6 +93,7 @@ function formatDateTime(iso: string): string {
       day:    "numeric",
       hour:   "2-digit",
       minute: "2-digit",
+      second: "2-digit",
     });
   } catch {
     return iso;
